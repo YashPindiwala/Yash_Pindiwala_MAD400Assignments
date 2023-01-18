9c3ad5715c6a145ca763712f5aba3af7d64f9cba
YashPindiwala <w0790912@myscc.ca>
2023-01-18 18:00:37 -0500

    interface changes

--- src/app/models/icontent.ts
+++ src/app/models/icontent.ts
@@ -1,9 +1,9 @@
 export interface IContent {
-    id: number
-    name: string
-    description: string
-    author: string
-    imgSrc?: string
-    type: string
-    tags?: string[]
+    id: number;
+    name: string;
+    description: string;
+    author: string;
+    imgSrc?: string;
+    type: string;
+    tags?: string[];
 }
