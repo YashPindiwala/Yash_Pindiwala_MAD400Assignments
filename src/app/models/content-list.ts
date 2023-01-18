7f24537830ccdd40433a7c91d3366efa0728724d
YashPindiwala <w0790912@myscc.ca>
2023-01-18 16:27:59 -0500

    Class ContentList Updated
    Added properties and methods to the ContentList Class

--- src/app/models/content-list.ts
+++ src/app/models/content-list.ts
@@ -1,2 +1,18 @@
+import { IContent } from "./icontent";
+
 export class ContentList {
+    private _arrayIcontent: IContent[];
+    constructor(){
+        this._arrayIcontent = []
+    }
+
+    public get content(){
+        return this._arrayIcontent;
+    }
+    public addContent(iContent:IContent) {
+        this._arrayIcontent.push(iContent);
+    }
+    public length(){
+        return this._arrayIcontent.length;
+    }
 }
