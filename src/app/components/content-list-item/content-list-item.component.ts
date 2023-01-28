b11db94bb252445e88b92e8b4e17a80b41964157
YashPindiwala <w0790912@myscc.ca>
2023-01-28 11:15:34 -0500

    accepting data from the tag
    sending data from tag to the list item component

--- src/app/components/content-list-item/content-list-item.component.ts
+++ src/app/components/content-list-item/content-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IContent } from 'src/app/models/icontent';
 
 @Component({
   selector: 'app-content-list-item',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-list-item.component.scss']
 })
 export class ContentListItemComponent {
-
+  @Input() singleItem?: IContent;
 }
