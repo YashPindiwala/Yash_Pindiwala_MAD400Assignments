f6f0d4eb8fb56606c539957c4aacf3faaec3bf8e
YashPindiwala <w0790912@myscc.ca>
2023-01-28 11:01:21 -0500

    Added Icontent array
    Added Icontent array in the contentlist class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,36 +8,5 @@ import { IContent } from './models/icontent';
 })
 export class AppComponent {
   title = 'Y_Pindiwala_MyFavouriteFoodItems';
-  
 
-  constructor(){
-    let item1: IContent = {
-      id: 10,
-      author: "Asaf Jha",
-      description: "It is a dish from hyderabad, made with the use of basmati rice, spices and meat.",
-      name: "Chicken biryani",
-      type: "Non-Veg"
-    };
-    let item2: IContent = {
-      id: 11,
-      author: "Kundan Lal Gujral",
-      description: "A curry made from chicken with spiced tomatoess and butter.",
-      name: "Butter Chicken",
-      type: "Non-Veg"
-    };
-    let item3: IContent = {
-      id: 12,
-      author: "Sui Dynasty",
-      description: "A dish of cooked rice that has been stir-fried in a wok.",
-      name: "Fried Rice",
-      type: "Veg"
-    };
-    let item4: IContent = {
-      id: 14,
-      author: "Alfredo di Leilo",
-      description: "A creamy pan sauce made from butter and cheese.",
-      name: "Alfredo Pasta",
-      type: "Veg"
-    };
-  }
 }
--- src/app/components/content-list/content-list.component.ts
+++ src/app/components/content-list/content-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IContent } from 'src/app/models/icontent';
 
 @Component({
   selector: 'app-content-list',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent {
+  items: IContent[] = [];
 
+  constructor(){let item1: IContent = {
+    id: 10,
+    author: "Asaf Jha",
+    description: "It is a dish from hyderabad, made with the use of basmati rice, spices and meat.",
+    name: "Chicken biryani",
+    type: "Non-Veg"
+  };
+  let item2: IContent = {
+    id: 11,
+    author: "Kundan Lal Gujral",
+    description: "A curry made from chicken with spiced tomatoess and butter.",
+    name: "Butter Chicken",
+    type: "Non-Veg"
+  };
+  let item3: IContent = {
+    id: 12,
+    author: "Sui Dynasty",
+    description: "A dish of cooked rice that has been stir-fried in a wok.",
+    name: "Fried Rice",
+    type: "Veg"
+  };
+  let item4: IContent = {
+    id: 14,
+    author: "Alfredo di Leilo",
+    description: "A creamy pan sauce made from butter and cheese.",
+    name: "Alfredo Pasta",
+    type: "Veg"
+  };
+    this.items.push(item1);
+    this.items.push(item2);
+    this.items.push(item3);
+    this.items.push(item4);
+  }
 }
