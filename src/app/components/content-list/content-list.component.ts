76a96da7840a6152fbf86dd405517585d5496981
YashPindiwala <w0790912@myscc.ca>
2023-01-28 12:01:14 -0500

    UI changes
    ui changes to the single list item

--- src/app/components/content-list/content-list.component.ts
+++ src/app/components/content-list/content-list.component.ts
@@ -14,7 +14,8 @@ export class ContentListComponent {
     author: "Asaf Jha",
     description: "It is a dish from hyderabad, made with the use of basmati rice, spices and meat.",
     name: "Chicken biryani",
-    type: "Non-Veg"
+    type: "Non-Veg",
+    imgSrc: "https://images.food52.com/McqpjxUiMekhfX6Rsq7wuuSoz0g=/2016x1344/filters:format(webp)/d815e816-4664-472e-990b-d880be41499f--chicken-biryani-recipe.jpg"
   };
   let item2: IContent = {
     id: 11,
@@ -28,14 +29,19 @@ export class ContentListComponent {
     author: "Sui Dynasty",
     description: "A dish of cooked rice that has been stir-fried in a wok.",
     name: "Fried Rice",
-    type: "Veg"
+    type: "Veg",
+    tags: [
+      "Rice",
+      "Vegetables"
+    ]
   };
   let item4: IContent = {
     id: 14,
     author: "Alfredo di Leilo",
     description: "A creamy pan sauce made from butter and cheese.",
     name: "Alfredo Pasta",
-    type: "Veg"
+    type: "Veg",
+    imgSrc: "https://thedefineddish.com/wp-content/uploads/2020/05/2020-04-24-00.51.42.jpg"
   };
     this.items.push(item1);
     this.items.push(item2);
