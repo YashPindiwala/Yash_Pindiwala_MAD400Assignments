4fbe683ed59ba1975ddd2a51abcf28de6ecedc7d
YashPindiwala <w0790912@myscc.ca>
2023-02-12 18:23:21 -0500

    search completed
    search with input completed, and will show error for not available id

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IContent } from 'src/app/models/icontent';
+import { FoodRecipeService } from 'src/app/services/food-recipe.service';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
+  searchItem?: IContent
+  constructor(private service: FoodRecipeService){
 
+  }
+  showItem(id: number){
+    this.searchItem = undefined;
+    this.service.getSingleContent(id).subscribe((value: IContent) => {
+      this.searchItem = value;
+    });
+  }
 }
