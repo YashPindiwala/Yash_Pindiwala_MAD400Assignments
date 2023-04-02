696f77a5a77422aaf768429edeec73abfdb0149e
YashPindiwala <w0790912@myscc.ca>
2023-04-02 14:28:07 -0400

    updated the service to perform CRUD

--- src/app/Service/food-item.service.ts
+++ src/app/Service/food-item.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api';
 import { Observable } from 'rxjs';
 import { CONTENTARRAY } from '../data/mock-content';
+import { IContent } from '../models/icontent';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,42 @@ export class FoodItemService implements InMemoryDbService {
     return {
       CONTENTARRAY
     }
+  }
+
+  getContent(): IContent[]{
+    return CONTENTARRAY;
+  }
+  // return single item
+  getSingleContent(id: number): any{
+    for (const iterator of CONTENTARRAY) {
+      if (iterator.id === id){
+        return iterator;
+      }
+    }
+    return null;
+  }
+  // Add to array
+  addContent(obj: IContent){
+    CONTENTARRAY.push(obj);
+  }
+  // Update Array
+  updateContent(obj: IContent): IContent[]{
+    for (const iterator in CONTENTARRAY) {
+      if (CONTENTARRAY[iterator].id === obj.id){
+        CONTENTARRAY[iterator] = obj
+      }
+    }
+    return CONTENTARRAY;
+  }
+  // delete item from array
+  removeItemContent(value: number): any{
+    for (const iterator in CONTENTARRAY) {
+      if (CONTENTARRAY[iterator].id === value){
+        const index = CONTENTARRAY.indexOf(CONTENTARRAY[iterator]);
+        const removedItem = CONTENTARRAY.splice(index,1)
+        return removedItem[0];
+      }
+    }
+    return null;
   }
 }
